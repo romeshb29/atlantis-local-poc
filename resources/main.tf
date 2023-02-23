aa248e939c693b5be2812b6339aeed99c925fe18
Romesh Bhatewara <romesh.bhatewara@takeoff.com>
2023-02-23 21:04:53 +0530

    tf code added

---
+++ resources/main.tf
@@ -0,0 +1,9 @@
+module "container-vm_example_simple_instance" {
+  source  = "terraform-google-modules/container-vm/google//examples/simple_instance"
+  version = "3.1.0"
+
+  project_id = 'xyz'
+  subnetwork = 'subnet1'
+  subnetwork_project = 'xyz'
+  # insert the 4 required variables here
+}
